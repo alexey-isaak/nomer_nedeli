7f52f37cf64e638096dd6f677480335000676168
Alexey_I <gafar@xaker.ru>
2023-02-03 07:24:04 +0300

    Itogovy script

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ 
+namespace nomer_dnya_nedeli
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите номер дня недели: ");
+            int dayindex = int.Parse(Console.ReadLine());
+             
+    switch ( dayindex ) {
+        
+        case 6 :
+            Console.WriteLine( "Выходной (Суббота)" );
+            break;
+        case 7 :
+            Console.WriteLine( "Выходной (Воскресенье)" );
+            break;
+        default :
+            Console.WriteLine( "Будний день." );
+            break;
+      
+        }
+    }
+}
+}
+
